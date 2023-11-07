74789f2269c599fd35ccb4adfa59918dd233c17d
Afif Defriant <afif@jojonomic.com>
2023-11-07 17:16:09 +0700

    headers auth

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,9 +1,18 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import Cookies from 'universal-cookie'
 
 export const cookies = new Cookies()
 
 export const Request = axios.create({
     baseURL: 'http://localhost:8030',
-    withCredentials: true,
+})
+
+Request.interceptors.request.use(async (config: AxiosRequestConfig): Promise<any> => {
+    if (cookies.get('authorization')) {
+        config.headers = {
+            authorization: cookies.get('authorization'),
+        }
+    }
+
+    return config
 })
